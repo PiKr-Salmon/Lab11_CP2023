12ee10f791439445806b6e5d441ab242310a50cd
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2023-12-22 14:51:48 +0700

    lab11_1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    int count = 1 ;
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","w"};
+    cout << "Press Enter 3 times to reveal your future.";
+    do{
+    cin.get();
+    count++;
+    }while(count <= 3);
+    int x = rand() % 8 + 1;
+    cout << "you will get " << grade[x] << " in this 261102.";
+
+    return 0;   
+}
